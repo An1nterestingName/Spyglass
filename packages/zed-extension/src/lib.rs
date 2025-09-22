0831c5e0b3b677be766775d774963839cee99965
Name <github@accounts.1nteresting.name>
2025-09-22 19:04:13 +0100

    Make constants &str instead of str

--- packages/zed-extension/src/lib.rs
+++ packages/zed-extension/src/lib.rs
@@ -1,8 +1,8 @@
 use std::fs;
 use zed_extension_api::{self as zed, Result};
 
-const GITHUB_REPO: str = "SpyglassMC/Spyglass";
-const BIN_NAME: str = "spyglass";
+const GITHUB_REPO: &str = "SpyglassMC/Spyglass";
+const BIN_NAME: &str = "spyglass";
 
 struct SpyglassExtension {
     cached_binary_path: Option<String>,
